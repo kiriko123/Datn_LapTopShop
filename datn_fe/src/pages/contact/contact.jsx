fb9db97fa29a2ab7a727cdf7caf60208893567e3
Le Van Sy <syle190104@gmail.com>
2024-12-15 22:19:00 +0700

    translate page contact

--- datn_fe/src/pages/contact/contact.jsx
+++ datn_fe/src/pages/contact/contact.jsx
@@ -19,17 +19,17 @@ const Contact = () => {
                     className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 bg-cyan-700 w-full max-w-4xl p-8 sm:p-12 rounded-xl shadow-lg text-white overflow-hidden">
                     <div className="flex flex-col space-y-8 justify-between">
                         <div>
-                            <h1 className="font-bold text-4xl tracking-wide">Contact Us</h1>
-                            <p className="pt-2 text-cyan-100 text-sm">Hello! If you have any questions or need answers, please contact us via the contact methods below.</p>
+                            <h1 className="font-bold text-4xl tracking-wide">Liên hệ với chúng tôi</h1>
+                            <p className="pt-2 text-cyan-100 text-sm">Chào bạn! Nếu bạn có bất kỳ câu hỏi nào hoặc cần được giải đáp, vui lòng liên hệ với chúng tôi qua các phương thức liên hệ dưới đây.</p>
                         </div>
                         <div className="flex flex-col space-y-6 ">
                             <div className="inline-flex space-x-2 items-center">
                                 <MdCall className="text-teal-300 text-xl"/>
-                                <span>+(123) 456 7890</span>
+                                <span>+(84) 8813 598</span>
                             </div>
                             <div className="inline-flex space-x-2 items-center">
                                 <MdMarkEmailUnread className="text-teal-300 text-xl"/>
-                                <span>contact@xyzwebsite.com</span>
+                                <span>laptopshop2024@gmail.com</span>
                             </div>
                             <div className="inline-flex space-x-2 items-center">
                                 <MdLocationOn className="text-teal-300 text-xl"/>
@@ -49,29 +49,29 @@ const Contact = () => {
                         <div className="relative z-10 bg-white rounded-xl shadow-lg p-8 text-gray-600 md:w-80">
                             <form action="" className="flex flex-col space-y-4">
                                 <div>
-                                    <label htmlFor="" className="text-sm">Your name</label>
+                                    <label htmlFor="" className="text-sm">Họ và tên</label>
                                     <input type="text"
-                                           placeholder="Your name"
+                                           placeholder="Nhập họ tên"
                                            className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300"/>
                                 </div>
 
                                 <div>
-                                    <label htmlFor="" className="text-sm">Email Address</label>
+                                    <label htmlFor="" className="text-sm">Email</label>
                                     <input type="email"
-                                           placeholder="Email Address"
+                                           placeholder="Nhập email"
                                            className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300"/>
                                 </div>
 
                                 <div>
-                                    <label htmlFor="" className="text-sm">Message</label>
+                                    <label htmlFor="" className="text-sm">Câu hỏi</label>
                                     <textarea
-                                        placeholder="Message"
+                                        placeholder="Nhập câu hỏi"
                                         rows="4"
                                         className="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none focus:ring-2 focus:ring-teal-300"></textarea>
                                 </div>
                                 <button
-                                    className="inline-block self-end bg-cyan-700 text-white font-bold rounded-lg px-6 py-2 uppercase text-sm">SEND
-                                    MESSAGE
+                                    className="inline-block self-end bg-cyan-700 text-white font-bold rounded-lg px-6 py-2 uppercase text-sm">
+                                    Gửi
                                 </button>
                             </form>
                         </div>
